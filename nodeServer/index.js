314e00678810dd50b32f00b74fdf82deae7731da
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-22 23:47:45 +0500

    created backend nodeserver folder and install socket.io dependencies

---
+++ nodeServer/index.js
@@ -0,0 +1,19 @@
+// Node server which will handle socket io connection 
+
+
+
+const io =require("socket.io")(3030);
+
+const users = {};
+
+// io.on mean listen every user who is connect who is sending message
+
+// socket.on is used to check what should if someone is connected 
+
+
+io.on('connection',socket =>{
+socket.on('new-user-joined',name =>{
+users[socket.id] = name;
+socket.broadcast.emit('user-joined',name);
+})
+})
