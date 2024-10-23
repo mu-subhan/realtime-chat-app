eca8d01d3ddc66820c7c7699263daa8e5506f196
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-23 11:59:41 +0500

    complete the project

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,24 +1,35 @@
 // Node server which will handle socket io connection 
 
 
-
-const io =require("socket.io")(3030);
-
-const users = {};
-
 // io.on mean listen every user who is connect who is sending message
 
 // socket.on is used to check what should if someone is connected 
 
-
-io.on('connection',socket =>{
-socket.on('new-user-joined',name =>{
-users[socket.id] = name;
-socket.broadcast.emit('user-joined',name);
-})
-
-socket.on('send',message =>{
-    socket.broadcast.emit("receive",{message:message,name:user[socket.id]})
+const io = require("socket.io")(8000,{
+    cors: {
+        origin: "http://127.0.0.1:5500", // Allow requests from this origin
+        methods: ["GET", "POST"]         // Allow these HTTP methods
+      }
 });
 
-})
+const users = {};
+
+io.on('connection', socket => {
+    // Handle new user joining
+    socket.on('new-user-joined', name => {
+        console.log("new user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    // Handle message sending
+    socket.on('send', message => {
+        socket.broadcast.emit("receive", { message: message, name: users[socket.id] });
+    });
+
+    // Handle user disconnect
+    socket.on('disconnect', () => { // Fixed typo: `disconnect` should be a string
+        socket.broadcast.emit("left", users[socket.id]);
+        delete users[socket.id];
+    });
+});
